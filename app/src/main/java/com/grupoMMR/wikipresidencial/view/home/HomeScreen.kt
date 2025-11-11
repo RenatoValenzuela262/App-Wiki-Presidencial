14eade708c21d2b7b91874ade2fa43de0f90d6a9
RenatoV <re.valenzuelae@duocuc.cl>
2025-11-11 10:55:32 -0300

    Avances en clases del 5 de Noviembre

--- app/src/main/java/com/grupoMMR/wikipresidencial/view/home/HomeScreen.kt
+++ app/src/main/java/com/grupoMMR/wikipresidencial/view/home/HomeScreen.kt
@@ -1,27 +1,41 @@
 package com.grupoMMR.wikipresidencial.view.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.grupoMMR.wikipresidencial.R
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.grupoMMR.wikipresidencial.view.navigation.Candidato
+import com.grupoMMR.wikipresidencial.view.navigation.CandidatoDetalle
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = viewModel(),
     navController: NavController
 ){
-    //val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+    val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
 
     Scaffold { padding ->
         Column(
@@ -38,6 +52,24 @@ fun HomeScreen(
                 style = MaterialTheme.typography.titleLarge,
                 text = "Candidatos 2025"
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())
+            ) {
+                Button(
+                    onClick = {navController.navigate(Candidato)},
+                    modifier = Modifier.fillMaxWidth(),
+                ){
+                    Image(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .width(100.dp),
+                        painter = painterResource(id = R.drawable.candidato),
+                        contentDescription = "Candidato"
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = )
+                }
+            }
         }
     }
 }
