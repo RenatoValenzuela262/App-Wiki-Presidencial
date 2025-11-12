e587ad6368c63361c9899184f0049f385a8a9c6f
linobotto <lino.botto@correos.cl>
2025-11-12 12:46:48 -0300

    Se implementa LazyVerticalGrid y se mejora la UI

--- app/src/main/java/com/grupoMMR/wikipresidencial/view/home/HomeScreen.kt
+++ app/src/main/java/com/grupoMMR/wikipresidencial/view/home/HomeScreen.kt
@@ -1,18 +1,18 @@
 package com.grupoMMR.wikipresidencial.view.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -21,7 +21,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -54,7 +57,9 @@ fun HomeScreen(
             Spacer(modifier = Modifier.height(16.dp))
 
             val miLista = CandidatosRepository.listaCandidatos
-            LazyColumn(
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(miLista) { candidato ->
@@ -69,21 +74,26 @@ fun HomeScreen(
 fun CandidatoItem(candidato: Candidato, modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxWidth().height(250.dp)
     ) {
-        Row(
-            modifier = Modifier.padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
             Image(
                 painter = painterResource(id = candidato.foto),
                 contentDescription = "Foto de ${candidato.nombre}",
-                modifier = Modifier.size(80.dp)
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
             )
-            Spacer(modifier = Modifier.width(16.dp))
             Text(
                 text = candidato.nombre,
-                style = MaterialTheme.typography.bodyLarge
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(Color.Black.copy(alpha = 0.5f))
+                    .padding(vertical = 8.dp)
             )
         }
     }
